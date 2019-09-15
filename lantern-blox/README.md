4d8322bc7b47ba1d52db1f14ae9cac73c801e8bc
Markus Poeschl <Poeschl@users.noreply.github.com>
2019-09-15 09:41:21 +0100

    Add sample game

---
+++ lantern-blox/README.md
@@ -0,0 +1,10 @@
+# Lantern Blox
+
+A little game to test the finger detection on the lantern.
+
+## Set it up
+Open the Webpage located under `src/web/Lantern-blox.html` and you are ready to go.
+
+## Instructions
+
+Try to click on all appearing boxes on the screen. After the game your score is shown.
---
+++ lantern-blox/src/web/js/blox.js
@@ -0,0 +1,84 @@
+const maxX = $(window).width() - 100;
+const maxY = $(window).height() - 100;
+
+let creationDelay = 3000;
+const timeDecrease = 50;
+const doubleThreshold = creationDelay - (creationDelay / 3);
+let boxCount = 0;
+let userScore = 0;
+
+$(document).ready(function () {
+    creationLoop();
+});
+
+function creationLoop() {
+    createRandomBox();
+    if (creationDelay < doubleThreshold) {
+        createRandomBox();
+    }
+
+    if (creationDelay > 0) {
+        setTimeout(creationLoop, creationDelay);
+        creationDelay = creationDelay - 50;
+    } else {
+        alert("You scored " + userScore + " points!");
+    }
+}
+
+function createBox(x, y) {
+    const template = $(".template.box").clone();
+    const boxId = boxCount++;
+
+    template.removeClass("template");
+    template.css("top", y + "px");
+    template.css("left", x + "px");
+    template.attr("data-id", boxId);
+
+    template.click(function (event) {
+        const box = $(event.currentTarget);
+        const id =box.attr("data-id");
+        const timeoutId = box.attr("data-timeout-id");
+        clickBox(id, timeoutId);
+        box.unbind();
+    });
+
+    const timeoutId = setTimeout(function () {
+        showVanishWarning(boxId);
+    }, 1000);
+    template.attr("data-timeout-id", timeoutId);
+
+    $(".playground").append(template);
+}
+
+function createRandomBox() {
+    let randomX = Math.floor(Math.random() * maxX);
+    let randomY = Math.floor(Math.random() * maxY);
+
+    createBox(randomX, randomY)
+}
+
+function showVanishWarning(id) {
+    const box = $('.box[data-id=' + id + ']');
+    box.addClass("vanishing");
+    const timeoutId = setTimeout(function () {
+        //destroyBox(id)
+    }, 1000);
+    box.attr("data-timeout-id", timeoutId);
+}
+
+function destroyBox(id) {
+    const box = $('.box[data-id=' + id + ']');
+    box.removeClass("vanishing");
+    box.addClass("vanish");
+    box.one('animationend', function (e) {
+        box.remove();
+    });
+}
+
+function clickBox(id, timeoutId) {
+    clearTimeout(timeoutId);
+    destroyBox(id);
+    userScore++;
+}
+
+
