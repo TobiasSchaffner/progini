ebac70b44955110b32f8471e400352ce750e9039
Ziegler Michael (extern) <Michael.Ziegler@funkwerk-itk.com>
2019-10-05 18:31:24 +0200

    improves spelling; adds debug output

--- finger-control/fingercontrol.py
+++ finger-control/fingercontrol.py
@@ -1,10 +1,18 @@
+import cv2
+import datetime
+import mss
+import numpy as np
+import os
 import picamera
 import picamera.array
-import time
-import cv2
-import numpy as np
-import mss
 import pyautogui
+import sys
+import time
+
+# enable debug output
+debug=False
+debugSwitches = ['-v', 'debug', '--verbose', 'DEBUG']
+folderDebugImages = None
 
 # Camera resolution
 res_width=1280
@@ -25,31 +33,47 @@ height=340
 reflections_tresh=30
 movement_tresh=100000
 
+# Image processing iteration
+iteration = 0
+
 def init_camera(camera):
     camera.resolution = (res_width, res_height)
     time.sleep(3)
 
 def take_picture(camera):
-    cap=picamera.array.PiRGBArray(camera)
-    camera.capture(cap,format="bgr")
+    cap = picamera.array.PiRGBArray(camera)
+    camera.capture(cap, format="bgr")
 
     # We are only interested in the projection area
     result = cap.array[y_off:y_off+height, x_off:x_off+width]
-    return cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+    grayscale_photo = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+
+    if debug: outputDebugImage('grayscale_photo', grayscale_photo)
+
+    return grayscale_photo
 
 def removeBackground(camera):
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
-    _, camera = cv2.threshold(camera,100,255,cv2.THRESH_BINARY)
-    camera = cv2.erode(camera, kernel, 1)
-    return camera
+    _, backgroundRemoved = cv2.threshold(camera, 100, 255, cv2.THRESH_BINARY)
+
+    if debug: outputDebugImage('background_removed', backgroundRemoved)
+
+    eroded = cv2.erode(camera, kernel, 1)
+
+    if debug: outputDebugImage('background_removed_eroded', eroded)
+
+    return eroded
 
 def calcThresholdImage(old_image, new_image):
     img_neg = cv2.subtract(old_image, new_image)
     drawable_img = cv2.bitwise_not(img_neg)
 
     # Filter reflections with treshhold
-    _, filtered_neg = cv2.threshold(img_neg,reflections_tresh,255,cv2.THRESH_TOZERO)
-    return filtered_neg;
+    _, filtered_neg = cv2.threshold(img_neg, reflections_tresh, 255, cv2.THRESH_TOZERO)
+
+    if debug: outputDebugImage('filtered_neg', filtered_neg)
+
+    return filtered_neg
 
 def detectMovement(pixel_delta):
     # If there is a large difference there is something in the area.
@@ -85,18 +109,39 @@ def determineContourMaximumPoint(image):
         return bot
 
 def scalePositionToScreen(cam_position):
-    return (int(cam_position[0] / width * screen_width), int(cam_position[1] / height * scree_height))
+    x = cam_position[0] / width * screen_width
+    y = cam_position[1] / height * scree_height
+
+    return (int(x), int(y))
 
 def moveMouseTo(position):
     pyautogui.moveTo(position[0], position[1])
 
+def clickAt(position):
+    pyautogui.click(position[0], position[1])
+
+def startedInDebugMode():
+    for argument in sys.argv:
+        if argument in debugSwitches:
+            return True
+    return False
+
+def createDebugFolder():
+    folderDebugImages = 'debug_images_{}'.format(datetime.datetime.now())
+    os.mkdir(folderDebugImages)
+
+def outputDebugImage(name, image):
+    cv2.imwrite('{}/{}_{}'.format(folderDebugImages, name, iteration), image)
+
 def main():
+    debug = startedInDebugMode()
+    if debug: createDebugFolder()
+
     with picamera.PiCamera() as camera:
         init_camera(camera)
         img_old = take_picture(camera)
         index = 0
         while True:
-
             img_new = removeBackground(take_picture(camera))
 
             threshold_img = calcThresholdImage(img_old, img_new)
@@ -109,8 +154,12 @@ def main():
                 if fingertip is not None:
                     # Draw a circle at the point of the fingertip
                     cv2.circle(threshold_img, fingertip, 8, (0, 255, 0), -1)
-                    moveMouseTo(scalePositionToScreen(fingertip))                    
+
+                    if debug: outputDebugImage('threshold_img', threshold_img)
+
+                    moveMouseTo(scalePositionToScreen(fingertip))
             else:
                 img_old = img_new
 
+            iteration = iteration + 1
 main()
